cd13ca0282f16db23c2326bfe4cd29cd50c5b993
future-X <future-X>
2018-07-05 17:06:58 +0800

    mnist train

--- mnist_CNN_network.py
+++ mnist_CNN_network.py
@@ -5,6 +5,7 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 #设置默认会话（session）
 sess = tf.InteractiveSession()
@@ -76,22 +77,41 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction,"float"))
 #初始化变量#启动图（graph）
 sess.run(tf.initialize_all_variables())
 
-#训练
+#save models
+saver = tf.train.Saver(max_to_keep=4)
 
+#训练
 train_accuracy = np.empty([1])
-for i in range(20000):
+train_loss = np.empty([1])
+iteration = 20000
+start = time.time()
+
+for i in range(iteration):
 	batch = mnist.train.next_batch(50)
 	if i%100 == 0:
-		accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prob:1.0})
-		print("step %d, training accuracy %g"%(i, accuracy))
-		train_accuracy = np.append(train_accuracy, accuracy)
+		batch_accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prob:1.0})
+		batch_loss = cross_entropy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prob:1.0})
+		print("step %d, training accuracy %.5f, loss %.5f"%(i, batch_accuracy, batch_loss))
+		train_accuracy = np.append(train_accuracy, batch_accuracy)
+		train_loss = np.append(train_loss, batch_loss)
+		saver.save(sess, 'CNN_model/mnist-model', global_step=i)
 	train_step.run(feed_dict = {x:batch[0],y_:batch[1],keep_prob:0.5})
+finish = time.time()
+elapsed_time = finish - start
 
 total_accuracy = accuracy.eval(feed_dict = {x:mnist.test.images,y_:mnist.test.labels,keep_prob:1.0})
-print("test accuracy %g"%total_accuracy)
+print("test accuracy %.5f"%total_accuracy)
+
 eval_fig = plt.figure()
 plt.suptitle('minist CNN network tarin')
 loss_ax = eval_fig.add_subplot(2,1,1)
 accu_ax = eval_fig.add_subplot(2,1,2)
-accu_ax.plot(train_accuracy, 'k--')
+loss_ax.set_title('train_loss     elapsed time: %.5f'%elapsed_time, fontsize='x-small')
+accu_ax.set_title('train_accuracy', fontsize='x-small')
+loss_ax.plot(train_loss, 'r--')
+loss_ax.annotate('loss=%.5f'%train_loss[len(train_loss)-1], xy=(iteration/100, train_loss[len(train_loss)-1]), xytext=(-90,20), textcoords='offset points')
+accu_ax.plot(train_accuracy, 'b--')
+accu_ax.annotate('accuracy=%.5f'%total_accuracy, xy=(iteration/100, train_accuracy[len(train_accuracy)-1]), xytext=(-90,-20), textcoords='offset points')
+
+plt.savefig('CNN-train.png', dpi=400, bbox_inches='tight')
 plt.show()
--- mnist_softmax.py
+++ mnist_softmax.py
@@ -3,6 +3,10 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
 
 #设置默认会话（session）
 sess = tf.InteractiveSession()
@@ -26,13 +30,46 @@ init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
-#训练模型1000次
-for i in range(40000):
-	batch_xs, batch_ys = mnist.train.next_batch(100)
-	sess.run(train_step, feed_dict={x:batch_xs,y_:batch_ys})
-print(sess.run(W),sess.run(b))
-
 #评估模型
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction,"float"))
 print(sess.run(accuracy,feed_dict={x:mnist.test.images, y_:mnist.test.labels}))
+
+#save models
+saver = tf.train.Saver(max_to_keep=4)
+
+#训练
+train_accuracy = np.empty([1])
+train_loss = np.empty([1])
+iteration = 20000
+start = time.time()
+
+for i in range(iteration):
+	batch = mnist.train.next_batch(50)
+	if i%100 == 0:
+		batch_accuracy = sess.run(accuracy, feed_dict={x:batch[0],y_:batch[1]})
+		batch_loss = sess.run(cross_entropy, feed_dict={x:batch[0],y_:batch[1]})
+		print("step %d, training accuracy %.5f, loss %.5f"%(i, batch_accuracy, batch_loss))
+		train_accuracy = np.append(train_accuracy, batch_accuracy)
+		train_loss = np.append(train_loss, batch_loss)
+		saver.save(sess, 'softmax_model/mnist-model', global_step=i)
+	sess.run(train_step, feed_dict = {x:batch[0],y_:batch[1]})
+finish = time.time()
+elapsed_time = finish - start
+
+total_accuracy = sess.run(accuracy, feed_dict = {x:mnist.test.images,y_:mnist.test.labels})
+print("test accuracy %.5f, elapsed-time %.5f"%(total_accuracy,elapsed_time))
+
+eval_fig = plt.figure()
+plt.suptitle('minist softmax regression tarin')
+loss_ax = eval_fig.add_subplot(2,1,1)
+accu_ax = eval_fig.add_subplot(2,1,2)
+loss_ax.set_title('train_loss     elapsed time: %.5f'%elapsed_time, fontsize='x-small')
+accu_ax.set_title('train_accuracy', fontsize='x-small')
+loss_ax.plot(train_loss, 'r--')
+loss_ax.annotate('loss=%.5f'%train_loss[len(train_loss)-1], xy=(iteration/100, train_loss[len(train_loss)-1]), xytext=(-90,20), textcoords='offset points')
+accu_ax.plot(train_accuracy, 'b--')
+accu_ax.annotate('accuracy=%.5f'%total_accuracy, xy=(iteration/100, train_accuracy[len(train_accuracy)-1]), xytext=(-90,-20), textcoords='offset points')
+
+plt.savefig('softmax-regression-train.png', dpi=400, bbox_inches='tight')
+plt.show()
