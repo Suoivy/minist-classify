62ef608daa860321a74acb663f2edece532eaff6
Suo-ivy <1183292322@qq.com>
2018-07-05 14:34:28 +0800

    modify CNN adding visualization

--- mnist_CNN_network.py
+++ mnist_CNN_network.py
@@ -3,6 +3,8 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
 
 #设置默认会话（session）
 sess = tf.InteractiveSession()
@@ -75,11 +77,21 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction,"float"))
 sess.run(tf.initialize_all_variables())
 
 #训练
+
+train_accuracy = np.empty([1])
 for i in range(20000):
 	batch = mnist.train.next_batch(50)
 	if i%100 == 0:
-		train_accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prob:1.0})
-		print("step %d, training accuracy %g"%(i, train_accuracy))
+		accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prob:1.0})
+		print("step %d, training accuracy %g"%(i, accuracy))
+		train_accuracy = np.append(train_accuracy, accuracy)
 	train_step.run(feed_dict = {x:batch[0],y_:batch[1],keep_prob:0.5})
 
-print("test accuracy %g"%accuracy.eval(feed_dict = {x:mnist.test.images,y_:mnist.test.labels,keep_prob:1.0}))
+total_accuracy = accuracy.eval(feed_dict = {x:mnist.test.images,y_:mnist.test.labels,keep_prob:1.0})
+print("test accuracy %g"%total_accuracy)
+eval_fig = plt.figure()
+plt.suptitle('minist CNN network tarin')
+loss_ax = eval_fig.add_subplot(2,1,1)
+accu_ax = eval_fig.add_subplot(2,1,2)
+accu_ax.plot(train_accuracy, 'k--')
+plt.show()
